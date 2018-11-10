6425d3505fc3316f48f9aed9ef1f8fd6a9387fbd
Unknown <sh_ibo@live.com>
2018-11-10 17:39:50 +0800

    Start

---
+++ app/src/main/java/chia/androidassignment/MovieRater.kt
@@ -0,0 +1,23 @@
+package chia.androidassignment
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+
+
+class MovieRater : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_rater)
+
+
+
+
+    }
+
+
+
+
+
+}
